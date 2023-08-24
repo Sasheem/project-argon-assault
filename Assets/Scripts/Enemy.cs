e634516d01e8515cb16dc38887b25f5e3aa8b4d5
Sasheem <sasheem.snell@gmail.com>
2023-08-24 14:04:35 -0400

    Logic for a score board

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,27 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start() {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     // fires upon collision from bullet
     void OnParticleCollision(GameObject other) {
+        ProcessHit();
+        KillEnemy();
         
+    }
+
+    void ProcessHit() {
+        // update scoreboard
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy() {
         // bring the enemy explosion effect to life and autmatically plays due to setting
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
 
