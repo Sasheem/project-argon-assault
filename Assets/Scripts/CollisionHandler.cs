cf92420ef92d57c819ced3130b8ed404067accca
Sasheem <sasheem.snell@gmail.com>
2023-08-24 11:37:22 -0400

    Trigger player explosion

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,14 +6,23 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
 
     void OnTriggerEnter(Collider other) {
         StartCrashSequence();
     }
 
     void StartCrashSequence() {
+        crashVFX.Play();
+        DisableChildrenMesh();
+        GetComponent<BoxCollider>().enabled = false;
         GetComponent<PlayerControls>().enabled = false;
         Invoke("ReloadLevel", loadDelay);
+    }
+
+    void DisableChildrenMesh() {
+        foreach (MeshRenderer meshInChild in GetComponentsInChildren<MeshRenderer>())
+            meshInChild.enabled = false;
     }
 
     void ReloadLevel() {
